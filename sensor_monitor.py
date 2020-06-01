581ed5cfc77eda536f07b8afdc5aac5a83303e96
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2020-06-01 15:52:55 +0200

    update sensor_monitor

--- sensor_monitor.py
+++ sensor_monitor.py
@@ -6,10 +6,11 @@ This script will monitor a sensor. If last measurement in the database is older 
 import argparse
 import smtplib
 import time
+import datetime
 from lib import my_env
 from lib import luft_class
 from lib.luft_store import *
-from email.mime.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipartc
 from email.mime.text import MIMEText
 
 
@@ -17,7 +18,7 @@ from email.mime.text import MIMEText
 parser = argparse.ArgumentParser(
     description="Monitor sensor."
 )
-parser.add_argument('-s', '--sensor', type=str, default='esp8266-72077',
+parser.add_argument('-s', '--sensor', type=str, default='sds011_sensor_13889',
                     help='Please provide the sensor id (as used on https://www.madavi.de/sensor/csvfiles.php)')
 args = parser.parse_args()
 cfg = my_env.init_env("luftdaten", __file__)
@@ -27,19 +28,20 @@ luft = luft_class.Luft()
 sensor = args.sensor
 max_ts = luft.latest_measurement(sensor)
 now = time.time()
-max_delta = int(os.getenv('DELTA')) * 60  # Delta time in seconds
+max_delta = 60 * 60 *24 # Delta time in seconds => 1 day
 # Send message only for limited amount of time.
 stop_msg = max_delta * 4
 delta = int(now - max_ts)
+delta_str = datetime.timedelta(seconds=delta)
 if (delta > max_delta) and (delta < stop_msg):
-    logging.info("Delta since last measurement: {} seconds, mail is prepared.".format(delta))
+    logging.info("Delta since last measurement: {}, mail is prepared.".format(delta_str))
     # Mail to user
     msg = MIMEMultipart()
     gmail_user = os.getenv('GMAIL_USER')
     gmail_pwd = os.getenv('GMAIL_PWD')
     recipient = os.getenv('RECIPIENT')
 
-    msg['Subject'] = 'Alert - LUFT measurement unavailable for {} seconds'.format(delta)
+    msg['Subject'] = 'Alert - LUFT measurement unavailable for {}'.format(delta_str)
     msg['From'] = gmail_user
     msg['To'] = recipient
     body = 'See Subject'
@@ -57,5 +59,6 @@ if (delta > max_delta) and (delta < stop_msg):
     server.quit()
 
 else:
-    logging.debug("Delta since last measurement: {} seconds, max_delta: {}".format(delta, max_delta))
+    logging.debug("Delta since last measurement: {}, max_delta: {}".format(delta_str,
+                                                                           str(datetime.timedelta(seconds=max_delta))))
 logging.info("End application")
